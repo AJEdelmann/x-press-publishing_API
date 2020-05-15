035e895bdb0529ea302c6d4b7892bbd560e2fec1
Albert Edelmann <albert.edelmann@gmail.com>
2020-05-15 12:46:12 +0200

    created Api Router

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorHandler = require('errorhandler');
 const morgan = require('morgan');
+const apiRouter = require('./api/api');
 
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use('/api', apiRouter);
+
 app.use(errorHandler());
 app.use(morgan('dev'));
 
