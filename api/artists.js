cb90ef3092140de7243ad76f063bc4a29e25e41f
Albert Edelmann <albert.edelmann@gmail.com>
2020-05-18 15:37:28 +0200

    created series Routes

--- api/artists.js
+++ api/artists.js
@@ -7,7 +7,7 @@ artistsRouter.param('artistId', (req, res, next, artistId) => {
     const sql = 'SELECT * FROM Artist WHERE Artist.id = $artistId';
     const values = {$artistId = artistId};
 
-    db.get(sql, values, (err, artists) => {
+    db.get(sql, values, (err, artist) => {
         if(err) {
             next(err);
         } else if (artist) {
